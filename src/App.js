665060be2b248d87e3e9c649d20f6dc12cf90d66
hiramhh <hiramhdez98@gmail.com>
2023-02-05 22:12:14 -0600

    la guia telefonica, paso 6

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Persons from "./components/Persons";
 import FilterName from "./components/FilterNames";
 import PersonForm from "./components/PersonForm";
+import axios from "axios";
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id:1},
-    { name: 'Ada Lovelace', number: '39-44-5323523', id:2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id:3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id:4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        setPersons(response.data);
+      })
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
     const checkName = persons.map(item => item.name).includes(newName);
-
     const checkNumber = persons.map(item => item.number).includes(newNumber);
     
-
     if(checkName || checkNumber){
       alert(`${newName} is already added to phonebook`);
       setNewName("");
@@ -65,7 +66,6 @@ function App() {
       <Persons 
       persons={persons}
       filter={filter}/>
-      debug: {filter}
     </div>
   );
   }
